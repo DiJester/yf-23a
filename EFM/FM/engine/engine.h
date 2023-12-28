2e1c8811ed0f8b9d3d63044b07f6946d9b4aea14
Dylan Liu <992748712@qq.com>
2023-12-28 20:52:40 +0800

    Create engine.h

---
+++ EFM/FM/engine/engine.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include "../common/vec3.h"
+#include "../common/force.h"
+
+namespace Aircraft {
+    class Engine {
+        public: 
+            Engine();
+            Engine(Vec3& pos_){
+                pos = pos_;
+            };
+            ~Engine();
+
+            inline Force getThrust(double throttle);
+            inline Vec3 getPos();
+            inline void setPos(Vec3& pos_);
+            inline double getRPM(double throttle);
+            inline double getRPMNorm();
+
+        private:
+            double maxThrustVal = 157000;
+            Vec3 pos = (0, 0, 0);
+    };
+
+    Force Engine::getThrust(double throttle) {
+        return Force{
+            force: (throttle * maxThrustVal, 0, 0 ),
+            pos: pos,
+        };
+    }
+
+    Vec3 Engine::getPos(){
+        return pos;
+    }
+
+    Vec3 Engine::setPos(Vec3& pos_){
+        pos = pos_;
+    }
+
+    double Engine::getRPM(double throttle){
+        return throttle * 3000;
+    }
+
+    double Engine::getRPMNorm(){
+        return  13000;
+    }
+
+}
+#endif
