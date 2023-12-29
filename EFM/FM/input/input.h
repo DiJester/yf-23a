676825af8c183a52c462b9f7aa4318329e614a5f
Dylan Liu <992748712@qq.com>
2023-12-29 18:13:39 +0800

    add axis to input.h

--- EFM/FM/input/input.h
+++ EFM/FM/input/input.h
@@ -1,123 +1,200 @@
-#pragma once
 #ifndef INPUT_H
 #define INPUT_H
 
-namespace Aircraft {
+#include "./axis/axis.h"
 
-enum Control {
-	//Axis
-	PITCH = 2001, //Stick pitch axis
-	ROLL = 2002, //Stick roll axis
-	YAW = 2003, //Pedals yaw axis
-	THROTTLE = 2004, //Throttle axis
-	GEAR_TOGGLE = 68, //68
-	GEAR_UP = 430, //
-	GEAR_DOWN = 431, //
-	BRAKE = 2111,
-	LEFT_BRAKE = 3162,
-	RIGHT_BRAKE = 3163,
-	FLAPS_INCREASE = 10001,
-	FLAPS_DECREASE = 10002,
-	FLAPS_DOWN = 145,
-	FLAPS_UP = 146,
-	FLAPS_TOGGLE = 72,
-	AIRBRAKE_EXTEND = 147,
-	AIRBRAKE_RETRACT = 148,
-	HOOK_TOGGLE = 69,
-	CONNECT_TO_CAT = 120,
-	NOSEWHEEL_STEERING_ENGAGE = 3133,
-	NOSEWHEEL_STEERING_DISENGAGE = 3134,
-	STARTER_BUTTON = 3013,
-	THROTTLE_DETEND = 3087,
+namespace Aircraft
+{
 
-	//Keyboard Stuff,
-	RUDDER_LEFT_START = 201,
-	RUDDER_LEFT_STOP = 202,
-	RUDDER_RIGHT_START = 203,
-	RUDDER_RIGHT_STOP = 204,
+	enum Control
+	{
+		// Axis
+		PITCH = 2001,	  // Stick pitch axis
+		ROLL = 2002,	  // Stick roll axis
+		YAW = 2003,		  // Pedals yaw axis
+		THROTTLE = 2004,  // Throttle axis
+		GEAR_TOGGLE = 68, // 68
+		GEAR_UP = 430,	  //
+		GEAR_DOWN = 431,  //
+		BRAKE = 2111,
+		LEFT_BRAKE = 3162,
+		RIGHT_BRAKE = 3163,
+		FLAPS_INCREASE = 10001,
+		FLAPS_DECREASE = 10002,
+		FLAPS_DOWN = 145,
+		FLAPS_UP = 146,
+		FLAPS_TOGGLE = 72,
+		AIRBRAKE_EXTEND = 147,
+		AIRBRAKE_RETRACT = 148,
+		HOOK_TOGGLE = 69,
+		CONNECT_TO_CAT = 120,
+		NOSEWHEEL_STEERING_ENGAGE = 3133,
+		NOSEWHEEL_STEERING_DISENGAGE = 3134,
+		STARTER_BUTTON = 3013,
+		THROTTLE_DETEND = 3087,
 
-	ROLL_LEFT_START = 197,
-	ROLL_LEFT_STOP = 198,
-	ROLL_RIGHT_START = 199,
-	ROLL_RIGHT_STOP = 200,
+		// Keyboard Stuff,
+		RUDDER_LEFT_START = 201,
+		RUDDER_LEFT_STOP = 202,
+		RUDDER_RIGHT_START = 203,
+		RUDDER_RIGHT_STOP = 204,
 
-	PITCH_DOWN_START = 193,
-	PITCH_DOWN_STOP = 194,
-	PITCH_UP_START = 195,
-	PITCH_UP_STOP = 196,
+		ROLL_LEFT_START = 197,
+		ROLL_LEFT_STOP = 198,
+		ROLL_RIGHT_START = 199,
+		ROLL_RIGHT_STOP = 200,
 
-	THROTTLE_UP_START = 1032,
-	THROTTLE_STOP = 1034,
-	THROTTLE_DOWN_START = 1033,
+		PITCH_DOWN_START = 193,
+		PITCH_DOWN_STOP = 194,
+		PITCH_UP_START = 195,
+		PITCH_UP_STOP = 196,
 
-	BRAKE_ALL_START = 10135,
-	BRAKE_ALL_STOP = 10136,
-	BRAKE_LEFT_START = 10180,
-	BRAKE_LEFT_STOP = 10181,
-	BRAKE_RIGHT_START = 10182,
-	BRAKE_RIGHT_STOP = 10183,
-	RADIO_MENU = 179, //iCommandToggleCommandMenu
-	RADIO_PTT = 10179,
+		THROTTLE_UP_START = 1032,
+		THROTTLE_STOP = 1034,
+		THROTTLE_DOWN_START = 1033,
 
-	//If the comms menu is changed these need to be found again.
-	LOCK_SLATS_RADIO_MENU = 966,
-	UNLOCK_SLATS_RADIO_MENU = 967,
-};
+		BRAKE_ALL_START = 10135,
+		BRAKE_ALL_STOP = 10136,
+		BRAKE_LEFT_START = 10180,
+		BRAKE_LEFT_STOP = 10181,
+		BRAKE_RIGHT_START = 10182,
+		BRAKE_RIGHT_STOP = 10183,
+		RADIO_MENU = 179, // iCommandToggleCommandMenu
+		RADIO_PTT = 10179,
 
-    class Input {
-        public:
-			Input() {};
-			~Input() {};
+		// If the comms menu is changed these need to be found again.
+		LOCK_SLATS_RADIO_MENU = 966,
+		UNLOCK_SLATS_RADIO_MENU = 967,
+	};
 
-            inline void setThrottle(double thrt);
-            inline void setStickPitch(double pitch);
-            inline void setStickRoll(double roll);
-            inline void setPedalYaw(double yaw);
+	class Input
+	{
+	public:
+		Input() : thrtAxis(0.015, -1, 1, 0, 1),
+				  pitchAxis(0.015, -1, 1, 0, 1),
+				  rollAxis(0.015, -1, 1, 0, 1),
+				  yawAxis(0.015, -1, 1, 0, 1),
+				  leftBrkAxis(0.02, 0, 1, 0, 0),
+				  rightBrkAxis(0.02, 0, 1, 0, 0)
+		{
+		}
+		~Input() {}
 
-            inline double getThrottle();
-            inline double getStickPitch();
-            inline double getStickRoll();
-            inline double getPedalYaw();
+		inline void setThrottle(double thrt);
+		inline void setStickPitch(double pitch);
+		inline void setStickRoll(double roll);
+		inline void setPedalYaw(double yaw);
 
-        private:
-            double throttle = 0;
-            double stickPitch = 0;
-            double stickRoll = 0;
-            double pedalYaw = 0;
-    };
+		inline double getThrottle();
+		inline double getStickPitch();
+		inline double getStickRoll();
+		inline double getPedalYaw();
+		inline void update();
 
-    void Input::setThrottle(double thrt){
-        throttle = thrt;
-    }
+		static inline double normalise(double value)
+		{
+			return (value + 1.0) / 2.0;
+		}
 
-    void Input::setStickPitch(double pitch){
-        stickPitch = pitch;
-    }
+		double getLeftBrk() const
+		{
+			return leftBrkAxis.getValue();
+		}
 
-    void Input::setStickRoll(double roll){
-        stickRoll = roll;
-    }
+		void setLeftBrk(double value)
+		{
+			leftBrkAxis.updateAxis(normalise(-value));
+		}
 
-    void Input::setPedalYaw(double yaw) {
-        pedalYaw = yaw;
-    }
+		double getYawDamper() const { return yawDamper; }
 
+		double getRightBrk() const
+		{
+			return rightBrkAxis.getValue();
+		}
 
-    double Input::getThrottle(){
-        return throttle;
-    }
+		void setRightBrk(double value)
+		{
+			rightBrkAxis.updateAxis(normalise(-value));
+		}
 
-    double Input::getStickPitch(){
-        return stickPitch;
-    }
+		double getPitchTrim() const { return pitchTrim; }
 
-    double Input::getStickRoll(){
-        return stickRoll;
-    }
+		double getRollTrim() const { return rollTrim; }
 
-    double Input::getPedalYaw(){
-        return pedalYaw;
-    }
+		double getYawTrim() const { return yawTrim; }
+
+	private:
+		Axis thrtAxis;
+		Axis pitchAxis;
+		Axis rollAxis;
+		Axis yawAxis;
+		Axis leftBrkAxis;
+		Axis rightBrkAxis;
+
+		double throttle = 0;
+		double stickPitch = 0;
+		double stickRoll = 0;
+		double pedalYaw = 0;
+		double leftBrk = 0;
+		double rightBrk = 0;
+		double yawDamper = 0;
+		double pitchTrim = 0;
+		double rollTrim = 0;
+		double yawTrim = 0;
+	};
+
+	void Input::setThrottle(double thrt)
+	{
+		thrtAxis.updateAxis(thrt);
+	}
+
+	void Input::setStickPitch(double pitch)
+	{
+		pitchAxis.updateAxis(pitch);
+	}
+
+	void Input::setStickRoll(double roll)
+	{
+		rollAxis.updateAxis(roll);
+	}
+
+	void Input::setPedalYaw(double yaw)
+	{
+		yawAxis.updateAxis(yaw);
+	}
+
+	double Input::getThrottle()
+	{
+		return thrtAxis.getValue();
+	}
+
+	double Input::getStickPitch()
+	{
+		return pitchAxis.getValue();
+	}
+
+	double Input::getStickRoll()
+	{
+		return rollAxis.getValue();
+	}
+
+	double Input::getPedalYaw()
+	{
+		return yawAxis.getValue();
+	}
+
+	void Input::update()
+	{
+		pitchAxis.update();
+		rollAxis.update();
+		yawAxis.update();
+		thrtAxis.update();
+		leftBrkAxis.update();
+		rightBrkAxis.update();
+	};
+
 }
 
-#endif
+#endif // INPUT_H
+
