c2b251583417a5c2f27717435954f8a8a1852748
Dylan Liu <992748712@qq.com>
2023-12-29 18:14:20 +0800

    add constructor to force.h

--- EFM/FM/common/force.h
+++ EFM/FM/common/force.h
@@ -4,10 +4,14 @@
 
 #include "vec3.h"
 
-struct Force{
-    Force(const Vec3& force_, const Vec3& pos_) :force(force_), pos(pos_){}
+struct Force
+{
+    Force(const Vec3 &force_, const Vec3 &pos_) : force(force_), pos(pos_) {}
+    Force() : force(), pos() {}
+
     Vec3 force;
     Vec3 pos;
 };
 
 #endif
+
