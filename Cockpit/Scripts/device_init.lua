3231c24b2f3177eb0b5247ca6f5e962ea26e9fc7
Dylan Liu <992748712@qq.com>
2024-03-14 16:56:33 +0800

    YF23A-21 Update device_init.lua

--- Cockpit/Scripts/device_init.lua
+++ Cockpit/Scripts/device_init.lua
@@ -10,6 +10,7 @@ attributes = {
 creators = {}
 creators[devices.DEMO] = { 'avLuaDevice', LockOn_Options.script_path .. 'Systems/demo.lua' }
 creators[devices.RUDDER] = { 'avLuaDevice', LockOn_Options.script_path .. 'Systems/rudder.lua' }
+creators[devices.RUDDER2] = { 'avLuaDevice', LockOn_Options.script_path .. 'Systems/rudder2.lua' }
 ---------------------------------------------
 dofile(LockOn_Options.common_script_path .. "KNEEBOARD/declare_kneeboard_device_left.lua")
 ---------------------------------------------
